07b383cb602b23a0c819646e5279187d09346993
Brad <brad.boutaugh@gmail.com>
2020-07-08 21:17:16 -0500

    initial commit

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -6,6 +6,23 @@ public class Person
     //Attributes: x-position, y-position, isSick
 
     private boolean isSick;
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setXCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setYCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
     private int xCoordinate;
     private int yCoordinate;
 
@@ -23,6 +40,21 @@ public class Person
     public void infect(Person person)
     {
         person.isSick = true;
+    }
+
+    public int sneeze(Person person)
+    {
+        int contactCoordinate = -1;
+        for(int i = 0; i<3;i++)
+        {
+            int currentXCoordinate = 0;
+            currentXCoordinate += i;
+            if(person.getXCoordinate() == currentXCoordinate)
+            {
+                contactCoordinate = person.getXCoordinate();
+            }
+        }
+        return contactCoordinate;
     }
 
     //Exhale function
--- src/com/company/Tests/PersonTest.java
+++ src/com/company/Tests/PersonTest.java
@@ -6,14 +6,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest
 {
-    Person person = new Person(false, 0, 0);
-    Person infectee = new Person(true, 5, 5);
+
     @Test
     public void infectTest()
     {
+        Person person = new Person(false, 2, 2);
+        Person infectee = new Person(true, 0, 0);
         infectee.infect(person);
         assertTrue(person.isSick(), "test");
 
+    }
+
+    @Test
+    public void sneezeContactTest()
+    {
+        Person person = new Person(false, 2, 2);
+        Person infectee = new Person(true, 0, 0);
+        assertEquals(infectee.getXCoordinate()+2, infectee.sneeze(person));
+    }
+
+    @Test
+    public void sneezeNoContactTest()
+    {
+        Person person = new Person(false, 5, 5);
+        Person infectee = new Person(true, 0, 0);
+        assertEquals(-1, infectee.sneeze(person));
     }
 
 }
